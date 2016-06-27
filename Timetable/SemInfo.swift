78e67bc5de33fb79b90c7049fa68d62fa49acec5
thucnhi2411 <let@lafayette.edu>
2016-06-27 20:58:32 +0700

    Add sem info

---
+++ Timetable/SemInfo.swift
@@ -0,0 +1,17 @@
+//
+//  SemInfo.swift
+//  Timetable
+//
+//  Created by THUC NHI on 6/27/16.
+//  Copyright © 2016 Nguyen Huy. All rights reserved.
+//
+
+import UIKit
+
+class SemInfo: UIViewController {
+
+    @IBAction func SemesterChoice(sender: UISegmentedControl) {
+    }
+    
+    @IBOutlet weak var academicYear: UITextField!
+}
